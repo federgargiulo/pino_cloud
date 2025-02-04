054010819dd8db36ab13b4712304d29679b37fb5
Salvatore Zito <salvatore.zito@vtsolutions.it>
2025-02-04 13:51:21 +0100

    managed server info

--- pliot-ui/src/app/service/equipment.service.ts
+++ pliot-ui/src/app/service/equipment.service.ts
@@ -1,37 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Equipment } from '../equipment';
+
+import { Location, LocationStrategy } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EquipmentService {
+  
+  static readonly BASE_DEV_URL  ="http://localhost:8080";
+  static readonly BASE_PROD_URL ="http://localhost:8080";
+  static readonly BASE_EQUIPMENT_RESOURCE ="/equipments";
 
-  url = 'http://localhost:8080/equipments';
+  url = "" ;
 
-  constructor() { }
+  constructor( location: Location , locationStrateg : LocationStrategy) { 
+     
+    console.log( "location path " + location.path() )
+    console.log( "location Strateg " + locationStrateg.path() )
+    console.log( "location getBaseHref " + locationStrateg.getBaseHref() )
+    
+    const baseUrl = isDevMode() ? EquipmentService.BASE_DEV_URL : EquipmentService.BASE_PROD_URL;
 
-   equipments: Equipment[] =[
-        {
-        equipmentId: "a",
-        status: "s",
-        name: "s",
-        version: "string"
-        },
-       {
-        equipmentId: "b",
-        status: "string",
-        name: "string",
-        version: "string"
-      },
-      {
-        equipmentId: "3b",
-        status: "erewr",
-        name: "strirng",
-        version: "string"
-      }
-   ]
+    this.url = baseUrl + EquipmentService.BASE_EQUIPMENT_RESOURCE;
+    
+    console.log( "Endpoint " + this.url );
+        
+  }
 
-   async getAllEquipments(): Promise<Equipment[]> {
+  
+
+  async getAllEquipments(): Promise<Equipment[]> {
       const data = await fetch(this.url);
       return await data.json() ?? [];
   }
