8729e50572fbbe297e105b5ff2b1e90f8440a4d9
Salvatore Zito <salvatore.zito@vtsolutions.it>
2025-02-08 18:58:23 +0100

    init module app

--- pliot-ui/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { EquipmentDetailComponent } from './equipment/equipment-detail.component';
-
-export const routes: Routes = [
-
- ];
--- pliot-ui/src/main.ts
+++ pliot-ui/src/main.ts
@@ -1,16 +1,7 @@
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/routes';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent,
+import { AppModule } from './app/app.module';
 
-  {
-    providers: [
-        provideProtractorTestingSupport(),
-        provideRouter(routeConfig)
-    ]
-  }
-)
-  .catch((err) => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
