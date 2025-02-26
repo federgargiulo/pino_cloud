012af6fdf1eb87aee2614e59c935c31b03d3268b
Salvatore Zito <salvatore.zito@vtsolutions.it>
2025-02-26 19:54:19 +0100

    added dashboard list

--- pliot-ui/src/app/dashboard/userdashboard-list/userdashboard-list.component.ts
+++ pliot-ui/src/app/dashboard/userdashboard-list/userdashboard-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from 'express';
 import { UserDashboardService } from '../../service/user-dashboard.service';
 
@@ -9,17 +9,31 @@ import { UserDashboardService } from '../../service/user-dashboard.service';
   styleUrl: './userdashboard-list.component.css'
 })
 
-
 export class UserdashboardListComponent implements OnInit {
+ 
+  dashBoardsList: any []= [];
 
-  dashBoardsList: any = [];
+  isLoading: boolean = true;
+   
 
- constructor( private userDashboardService: UserDashboardService ){} 
+ constructor( private userDashboardService: UserDashboardService ){
+    console.info( " sono nel costruttore ");
+ } 
 
   ngOnInit(): void {
     console.info( "load list ")
 
-    this.dashBoardsList = this.userDashboardService.getUserDashboards();
+    this.userDashboardService.getUserDashboards().subscribe(
+      {
+        next: (data) => {
+          this.dashBoardsList = data.body;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Errore nel caricamento dei dati', err);
+          this.isLoading = false;
+        }
+      });
    }
 
 
