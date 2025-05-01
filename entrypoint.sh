8d064016365cdef6372f51ec3b380d2ab9d9fc5c
salvatorezito <salvatore76@gmail.com>
2025-05-01 17:40:21 +0200

    entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 echo "JAVA_OPTS: $JAVA_OPTS"
 echo "SPRING_OPTS: $SPRING_OPTS"
 
+echo "🔍 Checking for UrlDecoder in app.jar..."
+jar tf /app/app.jar | grep UrlDecoder || echo "UrlDecoder not found."
+
+echo "🚀 Starting application..."
+
+
 exec java $JAVA_OPTS -jar /app/app.jar $SPRING_OPTS
