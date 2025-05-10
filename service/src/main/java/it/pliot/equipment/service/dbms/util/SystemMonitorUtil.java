84f351c4cc0f571aed138e42dd146b23fded76ba
salvatorezito <salvatore76@gmail.com>
2025-05-10 14:14:40 +0200

    intermedio

---
+++ service/src/main/java/it/pliot/equipment/service/dbms/util/SystemMonitorUtil.java
@@ -0,0 +1,32 @@
+package it.pliot.equipment.service.dbms.util;
+
+import it.pliot.equipment.io.SystemMonitorTO;
+import it.pliot.equipment.model.SystemMonitor;
+
+public class SystemMonitorUtil extends BaseConvertUtil<SystemMonitor, SystemMonitorTO> {
+
+
+    private static SystemMonitorUtil INSTANCE = new SystemMonitorUtil();
+
+    @Override
+    public SystemMonitor io2data(SystemMonitorTO in) {
+        SystemMonitor out = new SystemMonitor();
+        out.setDate(in.getDate());
+        out.setMetrics( in.getMetrics() );
+        out.setValue( in.getValue() );
+        return out;
+    }
+
+    @Override
+    public SystemMonitorTO data2io(SystemMonitor in) {
+        SystemMonitorTO out = new SystemMonitorTO();
+        out.setDate(in.getDate());
+        out.setMetrics( in.getMetrics() );
+        out.setValue( in.getValue() );
+        return out;
+    }
+
+    public static SystemMonitorUtil instance() {
+       return INSTANCE;
+    }
+}
