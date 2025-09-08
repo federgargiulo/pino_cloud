4c8ec5e2b12e78b24d5bee98040bd123904419c6
giuseppe Nocerino <giuseppe.nocerino@psen.wbaoneit.com>
2025-09-08 12:13:22 +0200

    removed ale hlp

--- ai_ext/reloadmodel.py
+++
@@ -1,9 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import load_iris
-import joblib
-
-X, y = load_iris(return_X_y=True)
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
-
-joblib.dump(clf, "rf_motor.pkl")
